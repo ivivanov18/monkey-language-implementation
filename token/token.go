23ace528e255cfb48125851a3b80fab561216ccd
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2021-02-01 01:23:52 +0200

    - add module go-monkey - add const tokens - add tests - add implementation NextToken - add helper functions

--- token/token.go
+++ token/token.go
@@ -3,13 +3,32 @@ package token
 type TokenType string
 
 type Token struct {
-	Type TokenType
+	Type     TokenType
 	Litteral string
 }
 
 const (
 	ILLEGAL = "ILLEGAL"
-	EOF = "EOF"
+	EOF     = "EOF"
 
 	// Identifiers + litterals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
 )
